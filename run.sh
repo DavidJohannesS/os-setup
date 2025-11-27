1757b6c47d607c7acc644e71150d6a2e9997d416
DavidJohannesS <davidjohannesSD@gmail.com>
2025-11-27 16:41:33 +0100

    fixed config dirs missing and added disable_sysd

--- run.sh
+++ run.sh
@@ -6,38 +6,42 @@ echo "Welcome to OS-SETUP!"
 cmd="ansible-playbook full.yaml --tags"
 
 options=(
-    "Create user"
+    "Create user" #1
     "Set apt sources"
+    "Setup keyboard" #3
     "Install apt packages"
     "Setup steams nvidia dependencies"
-    "Configure lightdm"
+    "Configure lightdm" #6
     "Configure pulseaudio"
     "Install/Update Neovim"
-    "Setup smb mounts"
-    "Pull DotFM"
-    "Quit"
+    "Setup smb mounts" #9
+    "Disable smb mounts"
+    "Pull manageRepos"
+    "Quit" #12
 )
 function pulldotfm()
 {
     echo "Enter path to clone DotFM to (Default=/gitrepos/OS-SETUP-FINAL): "
     read -r dotfm_path
-    dotfm_path=${dotfm_path:-/gitrepos/OS-SETUP-FINAL/DotFM}
+    dotfm_path=${dotfm_path:-~/gitrepos/os-setup/manageRepos}
     mkdir -p $dotfm_path
     echo "Cloning DotFM"
-    git clone https://github.com/DavidJohannesS/DotFM.git $dotfm_path
+    git clone https://github.com/DavidJohannesS/manageRepos.git $dotfm_path
 }
 select opt in "${options[@]}";do
     case $REPLY in
         1) $cmd user ;;
         2) $cmd sources ;;
-        3) $cmd apt_install ;;
-        4) $cmd steam ;;
-        5) $cmd lightdm ;;
-        6) $cmd audio ;;
-        7) $cmd neovim ;;
-        8) $cmd systemd ;;
-        9) pulldotfm ;;
-        10) echo "Exiting..." && break ;;
+        3) $cmd keyboard ;;
+        4) $cmd apt_install ;;
+        5) $cmd steam ;;
+        6) $cmd lightdm ;;
+        7) $cmd audio ;;
+        8) $cmd neovim ;;
+        9) $cmd enable_sysd ;;
+        10)$cmd disable_sysd ;;
+        11) pulldotfm ;;
+        12) echo "Exiting..." && break ;;
         *) echo "Invalid" ;;
     esac
 done
